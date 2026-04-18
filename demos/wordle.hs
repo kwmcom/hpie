131d4283f434fd27e511ec3fb1ef194cf52af6dd
kwmcom <kwimpnet@proton.me>
2026-04-18 14:44:03 +0800

    Add length guard to Wordle hint logic

--- demos/wordle.hs
+++ demos/wordle.hs
@@ -3,27 +3,28 @@ Set attempts to 0
 Set max to 5
 
 To define get_hint(guess, secret):
-    Set s_list to Call split(secret, "")
-    Set g_list to Call split(guess, "")
-    Set hint to ""
-    Set i to 0
-    
-    While i is less than 4:
-        Set g_char to Call get_item(g_list, i)
-        Set s_char to Call get_item(s_list, i)
-        
-        If g_char is s_char then:
-            Set hint to hint and g_char
-        Otherwise:
-            # Check for yellow (misplaced)
-            If Call contains(s_list, g_char) is 1.0 then:
-                Set hint to hint and "?"
+    Set g_len to Call length(guess)
+    If g_len is 4 then:
+        Set s_list to Call split(secret, "")
+        Set g_list to Call split(guess, "")
+        Set hint to ""
+        Set i to 0
+        While i is less than 4:
+            Set g_char to Call get_item(g_list, i)
+            Set s_char to Call get_item(s_list, i)
+            
+            If g_char is s_char then:
+                Set hint to hint and g_char
             Otherwise:
-                Set hint to hint and "_"
-        
-        Increase i by 1
-    
-    Say "Hint: " and hint
+                # Check for yellow (misplaced)
+                If Call contains(s_list, g_char) is 1.0 then:
+                    Set hint to hint and "?"
+                Otherwise:
+                    Set hint to hint and "_"
+            Increase i by 1
+        Say "Hint: " and hint
+    Otherwise:
+        Say "Hint: Word must be 4 letters."
 
 Say "Welcome to Wordle-lite!"
 
